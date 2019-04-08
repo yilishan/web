ff17d86d8680345afde5aab210f9c42447714343
一里山 <yilishan_hp@foxmail.com>
2019-04-08 21:21:57 +0800

    add dataList

--- simpleReact/simple-react/src/App.js
+++ simpleReact/simple-react/src/App.js
@@ -7,29 +7,74 @@ import {
 	Tree,
 } from 'antd';
 
-const { TreeNode } = Tree;
+// input obj, return TreeNode of Tree
+function AppTree(props) {
 
+}
+
+const { TreeNode } = Tree;
 class App extends Component {
-	constructor(){
+	constructor() {
 		super();
 		this.state = {
 			data: {
 				'学科': {
-					'数学':{
+					'数学': {
 						'老高1': '59',
 						'老高2': '58',
 						'老高3': '57',
 					},
-					'物理':{
+					'物理': {
 						'老杨1': '59',
 						'老杨2': '58',
 					},
-					'英语':{
+					'英语': {
 						'小明1': '59',
 						'小明2': '58',
 					},
 				},
 			},
+			dataList: [
+				{
+					title: '学科',
+					children: [
+						{
+							title: '语文',
+							children: [
+								{
+									title: '现代文',
+									children: [
+
+									]
+								},
+								{
+									title: '文言文',
+									children: [
+
+									]
+								},
+							]
+						},
+						{
+							title: '数学',
+							children: [
+								{
+									title: '几何',
+									children: [
+
+									]
+								},
+								{
+									title: '代数',
+									children: [
+
+									]
+								},
+							]
+						},
+					]
+				},
+			],
 		};
 	}
 
@@ -37,39 +82,28 @@ class App extends Component {
 		return (
 			<div className="App">
 				<header className="App-header">
-					{/* <img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React now123
-					</a> */}
-					<Button type="primary" onClick = {() => output(this.state.data)}> show </Button>
-					<Tree 
+					<Button type="primary" onClick={() => output(this.state.data)}> show </Button>
+					{/* <Tree
 						showLine
-						defaultExpandedKeys = {['0-0-0']}
-						onSelect = {this.onSelect}
+						defaultExpandedKeys={['0-0-0']}
+						onSelect={this.onSelect}
 					>
-						<TreeNode title="parent 1" key="0-0">
-						<TreeNode title="parent 1-0" key="0-0-0">
-							<TreeNode title="leaf" key="0-0-0-0" />
-							<TreeNode title="leaf" key="0-0-0-1" />
-							<TreeNode title="leaf" key="0-0-0-2" />
+						<TreeNode title='root' key="0-0">
+							<TreeNode title="parent 1-0" key="0-0-0">
+								<TreeNode title="leaf" key="0-0-0-0" />
+								<TreeNode title="leaf" key="0-0-0-1" />
+								<TreeNode title="leaf" key="0-0-0-2" />
+							</TreeNode>
+							<TreeNode title="parent 1-1" key="0-0-1">
+								<TreeNode title="leaf" key="0-0-1-0" />
+							</TreeNode>
+							<TreeNode title="parent 1-2" key="0-0-2">
+								<TreeNode title="leaf" key="0-0-2-0" />
+								<TreeNode title="leaf" key="0-0-2-1" />
+							</TreeNode>
 						</TreeNode>
-						<TreeNode title="parent 1-1" key="0-0-1">
-							<TreeNode title="leaf" key="0-0-1-0" />
-						</TreeNode>
-						<TreeNode title="parent 1-2" key="0-0-2">
-							<TreeNode title="leaf" key="0-0-2-0" />
-							<TreeNode title="leaf" key="0-0-2-1" />
-						</TreeNode>
-						</TreeNode>
-					</Tree>
+					</Tree> */}
+					<AppTree value={this.state.dataList} />
 				</header>
 			</div>
 		);
@@ -77,14 +111,14 @@ class App extends Component {
 }
 
 // 将对象展开成目录树
-function treeList(arr, obj, str){
-	if(obj){
-		for(let i in obj){
+function treeList(arr, obj, str) {
+	if (obj) {
+		for (let i in obj) {
 			arr.push(str + i);
-			if(typeof obj[i] === 'object'){
+			if (typeof obj[i] === 'object') {
 				treeList(arr, obj[i], str + '|----');
-			}else{
-				arr.push(str+ '|----' + obj[i]);
+			} else {
+				arr.push(str + '|----' + obj[i]);
 			}
 		}
 		return arr;
@@ -92,9 +126,9 @@ function treeList(arr, obj, str){
 }
 
 // 输出目录树
-function output(obj){
+function output(obj) {
 	let resarr = treeList([], obj, '|----');
-	for(let i in resarr){
+	for (let i in resarr) {
 		console.log(resarr[i]);
 	}
 }
